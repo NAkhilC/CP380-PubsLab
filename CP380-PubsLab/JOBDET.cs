326de87d56dd41e5fb16b244f78b507c7bfb6ec5
Akhil Nallamothu <42518147+NAkhilC@users.noreply.github.com>
2021-07-30 15:14:12 -0400

    Add files via upload

---
+++ CP380-PubsLab/JOBDET.cs
@@ -0,0 +1,28 @@
+﻿namespace CP380_PubsLab
+{
+    internal class JOBDET
+    {
+        internal string name;
+
+        public string Desc { get;  set; }
+        public int Id { get;  set; }
+        public string Salesid { get; internal set; }
+    }
+    public class   EMPDET
+    {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public int jid { get; set; }
+    }
+    public class SALDET
+    {
+        public string Salesid { get; set; }
+        public string title { get; set; }
+    }
+    public class TITDET
+    {
+        public string Salesid { get; set; }
+        public string name { get; set; }
+    }
+
+}
---
+++ CP380-PubsLab/List.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CP380_PubsLab
+{
+    internal class List<T1, T2>
+    {
+        internal void Add(short id, string desc)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
